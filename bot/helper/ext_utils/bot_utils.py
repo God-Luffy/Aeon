3009f5092f518b77b7dab26ee55c5514408dcabc
God-Luffy <143315093+God-Luffy@users.noreply.github.com>
2023-12-05 11:11:51 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -142,17 +142,14 @@ async def fetch_user_tds(user_id, force=False):
     return {}
 
 
-def get_progress_bar_string(pct):
-    pct = float(str(pct).strip('%'))
+def progress_bar(pct):
+    if isinstance(pct, str):
+        pct = float(pct.strip('%'))
     p = min(max(pct, 0), 100)
-    cFull = int(p // 8)
-    cPart = int(p % 8 - 1)
+    cFull = int((p + 5)// 10)
     p_str = '■' * cFull
-    if cPart >= 0:
-        p_str += ['▤', '▥', '▦', '▧', '▨', '▩', '■'][cPart]
-    p_str += '□' * (12 - cFull)
-    return f"[{p_str}]"
-
+    p_str += '□' * (10 - cFull)
+    return p_str
 
 def source(self):
     return (sender_chat.title if (sender_chat := self.message.sender_chat) else self.message.from_user.username or self.message.from_user.id)
